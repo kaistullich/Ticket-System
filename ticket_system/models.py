6f90deff0dc6e05b199f3a27e5093dffb0819c84
kaistullich <kairstullich@gmail.com>
2017-04-03 20:06:18 -0700

    completed further app setup in models

--- ticket_system/models.py
+++ ticket_system/models.py
@@ -12,3 +12,22 @@ from wtforms.validators import InputRequired, Email, Length, EqualTo
 
 from ticket_system import app
 
+with open('ticket_system/config.json') as f:
+    config_f = json.load(f)
+
+app.secret_key = os.urandom(24)
+
+Bootstrap(app)
+
+app.config['DATABASE_FILE'] = config_f['DATABASE_FILE']
+
+app.config['SQLALCHEMY_DATABASE_URI'] = config_f['SQLALCHEMY_DATABASE_URI']
+
+app.config['SQLALCHEMY_ECHO'] = config_f['SQLALCHEMY_ECHO']
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config_f['SQLALCHEMY_TRACK_MODIFICATIONS']
+
+db = SQLAlchemy(app)
+
+admin = Admin(app, template_mode='bootstrap3')
+
