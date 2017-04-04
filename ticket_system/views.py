c89e28ee9e1ec951aad65115d4e6183ed7ab817e
kaistullich <kairstullich@gmail.com>
2017-04-04 09:12:17 -0700

    completed email notification -> views.py

--- ticket_system/views.py
+++ ticket_system/views.py
@@ -1,12 +1,15 @@
 import bcrypt
+import json
 from flask import flash, redirect, render_template, request, url_for
+from flask_mail import Message
+from ticket_system.models import MessageForm, TicketDB, Admin, db, app, mail
 
-from ticket_system import app
-from ticket_system.models import MessageForm, TicketDB, Admin, db
+with open('ticket_system/config.json') as f:
+    config_f = json.load(f)
 
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
     form = MessageForm()
     if form.validate_on_submit() and request.method == 'POST':
@@ -17,6 +20,15 @@ def home():
         new_ticket = TicketDB(name=name, email=email, message=message)
         db.session.add(new_ticket)
         db.session.commit()
+        msg = Message(subject='Ticket Received!',
+                      sender=config_f['MAIL_USERNAME'],
+                      recipients=[email],
+                      html='<p>Dear {name},</p>\
+                            <p>We have received your ticket and one of our dedicated team members \
+                            will work on this as soon as possible.</p>\
+                            Kind Regards,</p>\
+                            <p>fitBody Customer Support</p>'.format(name=name))
+        mail.send(msg)
         flash('Your tickets was successfully submitted!')
         return redirect(url_for('home'))
 
