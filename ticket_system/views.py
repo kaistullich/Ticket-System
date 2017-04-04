9dc005e652cf1b5208458b1f90998f198f4172d9
kaistullich <kairstullich@gmail.com>
2017-04-03 20:06:36 -0700

    home route set -> views

--- ticket_system/views.py
+++ ticket_system/views.py
@@ -3,3 +3,8 @@ from flask import Flask, redirect, render_template, request, session, url_for
 
 from ticket_system import app
 from ticket_system.models import *
+
+
+@app.route('/')
+def home():
+    return 'Hello Flask!'
