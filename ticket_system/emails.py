69a9e49e3fb0a1a0d0023072e6ff6e34bfd1f972
kaistullich <kairstullich@gmail.com>
2017-04-04 11:59:16 -0700

    setup Twilio Client -> emails.py

--- ticket_system/emails.py
+++ ticket_system/emails.py
@@ -7,6 +7,7 @@ from ticket_system import app
 from ticket_system.models import mail
 from .decorators import async_
 
+from twilio.rest import Client
 
 with open('ticket_system/config.json') as f:
     config_f = json.load(f)
@@ -30,3 +31,17 @@ def notification(c_name, c_email):
                [c_email],
                render_template("follower_email.html",
                                c_name=c_name))
+
+
+def twilioSMS(p_to, p_name):
+    # Twilio Auth
+    account_sid = config_f['account_sid']
+    auth_token = config_f['auth_token']
+
+    client = Client(account_sid, auth_token)
+
+    client.messages.create(
+        to=p_to,
+        from_="+16505390961",
+        body="Dear {name}, your ticket was received!".format(name=p_name)
+    )
