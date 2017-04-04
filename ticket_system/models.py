fdf637277537e574c0ab40ae8debcd3150c2c5b8
kaistullich <kairstullich@gmail.com>
2017-04-03 20:34:57 -0700

    created DB model and Message form -> models.py

--- ticket_system/models.py
+++ ticket_system/models.py
@@ -6,9 +6,8 @@ from flask_admin.contrib.sqla import ModelView
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, PasswordField
-from wtforms import fields, widgets
-from wtforms.validators import InputRequired, Email, Length, EqualTo
+from wtforms import StringField, TextAreaField
+from wtforms.validators import InputRequired, Email
 
 from ticket_system import app
 
@@ -16,18 +15,31 @@ with open('ticket_system/config.json') as f:
     config_f = json.load(f)
 
 app.secret_key = os.urandom(24)
-
 Bootstrap(app)
 
 app.config['DATABASE_FILE'] = config_f['DATABASE_FILE']
-
 app.config['SQLALCHEMY_DATABASE_URI'] = config_f['SQLALCHEMY_DATABASE_URI']
-
 app.config['SQLALCHEMY_ECHO'] = config_f['SQLALCHEMY_ECHO']
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = config_f['SQLALCHEMY_TRACK_MODIFICATIONS']
-
 db = SQLAlchemy(app)
-
 admin = Admin(app, template_mode='bootstrap3')
 
+
+class MessageForm(FlaskForm):
+    name = StringField('Name:', [InputRequired()])
+    email = StringField('Email:', [InputRequired(), Email('Invalid Email!')])
+    message = TextAreaField('Message:', [InputRequired()])
+
+
+class TicketDB(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    message = db.Column(db.String(500))
+
+
+class TicketAdminView(ModelView):
+    create_template = 'create.html'
+    edit_template = 'edit.html'
+
+admin.add_view(TicketAdminView(TicketDB, db.session))
