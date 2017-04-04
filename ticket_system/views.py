47da71da5681f93dccbfccc2687a912d112eb98b
kaistullich <kairstullich@gmail.com>
2017-04-03 20:51:22 -0700

    created backend in views for form submission

--- ticket_system/views.py
+++ ticket_system/views.py
@@ -1,11 +1,23 @@
 import bcrypt
-from flask import Flask, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session, url_for
 
 from ticket_system import app
 from ticket_system.models import MessageForm, TicketDB, Admin, db
 
 
 @app.route('/')
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    form = MessageForm()
+    if form.validate_on_submit() and request.method == 'POST':
+        name = form.name.data
+        email = form.email.data
+        message = form.message.data
+
+        new_ticket = TicketDB(name=name, email=email, message=message)
+        db.session.add(new_ticket)
+        db.session.commit()
+        flash('Your tickets was successfully submitted!')
+        return redirect(url_for('home'))
+
+    return render_template('home.html', form=form)
