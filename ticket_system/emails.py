14cd3007eb2e72de0b84ad997a7be7e8517e098f
kaistullich <kairstullich@gmail.com>
2017-04-04 09:40:04 -0700

    moved all email functions to seperate files

--- ticket_system/emails.py
+++ ticket_system/emails.py
@@ -1,9 +1,31 @@
+import json
+
+from flask import render_template
 from flask_mail import Message
+from threading import Thread
+from ticket_system import app
 from ticket_system.models import mail
 
 
-def send_email(subject, sender, recipients, text_body, html_body):
+with open('ticket_system/config.json') as f:
+    config_f = json.load(f)
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(subject, sender, recipients, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+
+
+def notification(c_name, c_email):
+    send_email("Support Ticket Received!",
+               config_f['MAIL_USERNAME'],
+               [c_email],
+               render_template("follower_email.html",
+                               c_name=c_name))
