d1ae5edc373c3e05d47a5e7c67bcd2b30a62a608
kaistullich <kairstullich@gmail.com>
2017-04-06 22:24:23 -0700

    added date/time for db.commit & fo loop to check severity/tix status

--- src/views.py
+++ src/views.py
@@ -4,11 +4,10 @@ from random import randrange
 
 from flask import flash, redirect, render_template, request, url_for
 
-from src.all_notifications import email_notification, twilio_sms
+from src.all_notifications import email_notification, twilio_sms, ticket_call
 from src.models import MessageForm, TicketDB, db, app
 
 from twilio.twiml.voice_response import VoiceResponse
-
 
 with open('src/config.json') as f:
     config_f = json.load(f)
@@ -48,6 +47,11 @@ def home():
 
             ticket = TicketDB.query.filter_by(ticketID=rand_num).first()
 
+            tickets = TicketDB.query.all()
+            for ticket in tickets:
+                if ticket.ticket_severity == 1:
+                    if ticket.ticket_status == "Open":
+                        ticket_call('6507876895')
             # email_notification(name, email, rand_num)
             # twilio_sms(number, name, ticket.ticketID)
 
@@ -63,17 +67,19 @@ def home():
             severity = form.severity.data
             message = form.message.data
             status = "Open"
-            tix_datetime = time.strftime('%H%M')
+            tix_date = time.strftime('%a, %d %b %Y')
+            tix_time = time.strftime('%H%M')
 
             new_ticket = TicketDB(ticketID=new_rand_num, name=name, email=email, ticket_group=tix_type,
-                                  ticket_severity=severity, message=message, ticket_status=status)
+                                  ticket_severity=severity, message=message, ticket_status=status, ticket_date=tix_date,
+                                  ticket_time=tix_time)
             db.session.add(new_ticket)
             db.session.commit()
 
             ticket = TicketDB.query.filter_by(ticketID=new_rand_num).first()
 
-            email_notification(name, email, rand_num)
-            twilio_sms(number, name, ticket.ticketID)
+            # email_notification(name, email, rand_num)
+            # twilio_sms(number, name, ticket.ticketID)
 
             flash('Your tickets was successfully submitted!', 'success')
             return redirect(url_for('home'))
@@ -82,14 +88,11 @@ def home():
 
 
 @app.route("/words", methods=['GET', 'POST'])
-def hello_monkey():
+def ticket_voice():
     """Respond to incoming requests."""
-    tickets = TicketDB.query.all()
-    for ticket in tickets:
-        if ticket.ticket_severity == 1:
-            if ticket.ticket_status == "Open":
-                print(ticket.ticketID)
+    # TODO: Put name of dept and ticket number into voice
     resp = VoiceResponse()
-    resp.say("")
+    resp.say("A new Priority 1 ticket has been created and assigned to the {name} team. Ticket number {t_num}. \
+             Please log in within 1 hour to respond to the ticket.".format(name=None, t_num=None), loop=3)
 
     return str(resp)
