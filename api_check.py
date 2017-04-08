ec9026373ae2526e4f4f48f2e57dbf50e48ae56f
kaistullich <kairstullich@gmail.com>
2017-04-07 22:38:36 -0700

    removed phone number and added config_f

--- api_check.py
+++ api_check.py
@@ -5,6 +5,8 @@ import requests
 
 from src.all_notifications import ticket_call
 
+with open('src/config.json') as f:
+    config_f = json.load(f)
 
 r = requests.get('http://127.0.0.1:5000/api/Ticket')
 print('Request status: ' + str(r.status_code))
@@ -15,6 +17,7 @@ time_now = int(time.strftime('%H%M'))
 while True:
     time.sleep(10)
     print('** CHECKING API FOR NEW TICKETS! **')
+    ticket_list = []
     for ticket in tickets['objects']:
         status = ticket['ticket_status']
         severity = ticket['ticket_severity']
@@ -24,7 +27,5 @@ while True:
                 if time_now - tix_time >= 60:
                     # TODO: Change number to cellphone when testing away from home
                     print('Ticket(s) found', ticket['ticketID'])
-                    ticket_call('4084655095')
-                print('No ticket found')
-            print('No ticket found')
-        print('No ticket found')
+                    ticket_list.append(ticket['ticketID'])
+                    ticket_call(config_f['dept_num'])  # Home number currently
