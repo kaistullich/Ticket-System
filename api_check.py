d3b6de831c15140e3e8ef9cab01bab07e400ecec
kaistullich <kairstullich@gmail.com>
2017-04-07 18:51:46 -0700

    changed varibale names api_check

--- api_check.py
+++ api_check.py
@@ -10,7 +10,7 @@ r = requests.get('http://127.0.0.1:5000/api/Ticket')
 print('Request status: ' + str(r.status_code))
 api_objs = r.text
 tickets = json.loads(api_objs)
-now = int(time.strftime('%H%M'))
+time_now = int(time.strftime('%H%M'))
 
 while True:
     time.sleep(10)
@@ -18,12 +18,12 @@ while True:
     for ticket in tickets['objects']:
         status = ticket['ticket_status']
         severity = ticket['ticket_severity']
-        time_ = ticket['ticket_time']
+        tix_time = ticket['ticket_time']
         if severity == 1:
             if status == "Open":
-                if now - time_ >= 60:
+                if time_now - tix_time >= 60:
                     # TODO: Change number to cellphone when testing away from home
-                    print('Ticket/s found', ticket['tickerID'])
+                    print('Ticket(s) found', ticket['ticketID'])
                     ticket_call('4084655095')
                 print('No ticket found')
             print('No ticket found')
