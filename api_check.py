9e200f1fe5786bec84f1ca9b4f95ad87703a954d
kaistullich <kairstullich@gmail.com>
2017-04-08 17:40:11 -0700

    changed for loop

--- api_check.py
+++ api_check.py
@@ -3,29 +3,43 @@ import time
 
 import requests
 
-from src.all_notifications import ticket_call
+from src.all_notifications import ticket_reminder_call
+# from src.views import ticket_reminder_route
 
 with open('src/config.json') as f:
     config_f = json.load(f)
-
-r = requests.get('http://127.0.0.1:5000/api/Ticket')
+    
+# TODO: change url to NEW ngrok url
+r = requests.get('https://8380b277.ngrok.io/api/Ticket')
 print('Request status: ' + str(r.status_code))
 api_objs = r.text
 tickets = json.loads(api_objs)
 time_now = int(time.strftime('%H%M'))
+date_now = time.strftime('%a, %d %b %Y')
 
 while True:
     time.sleep(10)
     print('** CHECKING API FOR NEW TICKETS! **')
-    ticket_list = []
+    open_tix_counter = 0
+    open_tix_found = False
     for ticket in tickets['objects']:
         status = ticket['ticket_status']
         severity = ticket['ticket_severity']
         tix_time = ticket['ticket_time']
+        tix_date = ticket['ticket_date']
         if severity == 1:
             if status == "Open":
-                if time_now - tix_time >= 60:
-                    # TODO: Change number to cellphone when testing away from home
+                if date_now == tix_date:
+                    if time_now - tix_time >= 60:
+                        print('Ticket(s) found', ticket['ticketID'])
+                        open_tix_counter += 1
+                        open_tix_found = True
+                elif date_now != tix_date:
                     print('Ticket(s) found', ticket['ticketID'])
-                    ticket_list.append(ticket['ticketID'])
-                    ticket_call(config_f['dept_num'])  # Home number currently
+                    open_tix_counter += 1
+                    open_tix_found = True
+
+    if open_tix_found:
+        # ticket_reminder_route(open_tix_counter)  # views.py
+        # TODO: Change number to cellphone when testing away from home
+        ticket_reminder_call(config_f['dept_num'])  # Home number currently
