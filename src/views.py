3cf718d9c4d8a1d311d26d33a85e0348a9999f80
kaistullich <kairstullich@gmail.com>
2017-05-06 18:15:41 -0700

    add 3 socketio.on events

--- src/views.py
+++ src/views.py
@@ -3,7 +3,7 @@ import time
 
 import bcrypt
 from flask import flash, redirect, render_template, request, url_for, session
-from flask_socketio import emit
+from flask_socketio import emit, join_room, leave_room
 from twilio.base.exceptions import TwilioRestException
 from twilio.twiml.voice_response import VoiceResponse
 
@@ -295,13 +295,53 @@ def ticket_creation():
     return str(resp)
 
 
+@app.route('/chat_login', methods=['GET', 'POST'])
+def chat_login():
+    """"Login form to enter a room."""
+    form = ChatLoginForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        session['room'] = form.room.data
+        return redirect(url_for('.chat'))
+    elif request.method == 'GET':
+        form.name.data = session.get('name', '')
+        form.room.data = session.get('room', '')
+    return render_template('chat_login.html', form=form)
+
+
 @app.route('/chat')
 def chat():
     form = ChatForm()
-    return render_template('chat.html', form=form)
+    """Chat room. The user's name and room must be stored in
+        the session."""
+    name = session.get('name', '')
+    room = session.get('room', '')
+    if name == '' or room == '':
+        return redirect(url_for('chat_login'))
+    return render_template('chat.html', name=name, room=room, form=form)
 
 
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    print('Received a message: ' + str(json))
-    emit('message', json, broadcast=True)
+@socketio.on('joined', namespace='/chat')
+def joined(message):
+    """Sent by clients when they enter a room.
+    A status message is broadcast to all people in the room."""
+    room = session.get('room')
+    join_room(room)
+    emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+
+
+@socketio.on('text', namespace='/chat')
+def text(message):
+    """Sent by a client when the user entered a new message.
+    The message is sent to all people in the room."""
+    room = session.get('room')
+    emit('message', {'msg': session.get('name') + ': ' + message['msg']}, room=room)
+
+
+@socketio.on('left', namespace='/chat')
+def left(message):
+    """Sent by clients when they leave a room.
+    A status message is broadcast to all people in the room."""
+    room = session.get('room')
+    leave_room(room)
+    emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
