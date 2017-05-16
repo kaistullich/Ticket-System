60dbb779a09eb5ee4ad54c6a7112aba0908cd573
kaistullich <kairstullich@gmail.com>
2017-05-16 15:35:13 -0700

    created protype for $.ajax request with posting comments

--- src/static/js/ticket-comments.js
+++ src/static/js/ticket-comments.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-   // Variable for the comment=append div
+    // Get Name of the user
+    var custName = $('#first-name').text();
+    // Variable for the comment=append div
     var commentAppend = $('#comment-append');
     // Check if the div is empty
     if (commentAppend.html() === '') {
@@ -7,12 +9,33 @@ $(document).ready(function () {
         commentAppend.append('<i><h4 id="com-placeholder">No comments so far...</h4></i>');
     }
     // On click of button
-    $('#submit-btn').click(function () {
+    $('form').on('submit', function (event) {
+        // Grab `box` ID
+        var divBox = document.getElementById("box");
+        // Auto scroll when messages exceed the height of the box
+        divBox.scrollTop = divBox.scrollHeight;
         // Grab input entered
         var comment = $('#comment').val();
         // Remove the placeholder comment
         $('#com-placeholder').remove();
-        // Append the message entered TODO: this will need to be the DB messages; static for now
-        commentAppend.append('<div class="msg_bubble">' + comment + '</div>')
+        // Start AJAX
+        $.ajax({
+            data: {
+                comment: comment
+            },
+            type: 'POST',
+            url: '/process_comments'
+        // On success of AJAX call
+        }).done(function (data) {
+            // Clear the textarea after new comment submitted
+            $('#comment').val('');
+            if (data.error) {
+                console.log(data.error)
+            } else {
+                // Append the message entered TODO: this will need to be the DB messages; static for now
+                commentAppend.append('<div class="msg_bubble"><strong>' + custName +': </strong>' + comment + '</div>');
+            }
+        });
+        event.preventDefault();
     });
-});
+}); // END $.ready()
