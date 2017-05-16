1b2869995df23fd016ef73233787f28f9c728b11
kaistullich <kairstullich@gmail.com>
2017-05-16 01:23:07 -0700

    moved ticket jQuery code to correct file

--- src/static/js/ticket-comments.js
+++ src/static/js/ticket-comments.js
@@ -1,3 +1,18 @@
-/**
- * Created by Kai on 5/16/17.
- */
+$(document).ready(function () {
+   // Variable for the comment=append div
+    var commentAppend = $('#comment-append');
+    // Check if the div is empty
+    if (commentAppend.html() === '') {
+        // If empty display message
+        commentAppend.append('<i><h4 id="com-placeholder">No comments so far...</h4></i>');
+    }
+    // On click of button
+    $('#submit-btn').click(function () {
+        // Grab input entered
+        var comment = $('#comment').val();
+        // Remove the placeholder comment
+        $('#com-placeholder').remove();
+        // Append the message entered TODO: this will need to be the DB messages; static for now
+        commentAppend.append('<div class="msg_bubble">' + comment + '</div>')
+    });
+});
