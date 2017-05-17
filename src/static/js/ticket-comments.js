6a77974ccb7c2a69af4efd55fab96c23786663b6
kaistullich <kairstullich@gmail.com>
2017-05-16 17:18:42 -0700

    comments

--- src/static/js/ticket-comments.js
+++ src/static/js/ticket-comments.js
@@ -2,34 +2,37 @@ $(document).ready(function () {
     // Get Name of the user
     var custName = $('#first-name').text();
 
-    // On click of button
+    // On submit of form
     $('form').on('submit', function (event) {
         // Grab `box` ID
         var divBox = document.getElementById("box");
         // Auto scroll when messages exceed the height of the box
         divBox.scrollTop = divBox.scrollHeight;
-        // Grab input entered
+        // Grab input entered in textarea
         var comment = $('#comment').val();
 
-        // Start AJAX
+        // AJAX request
         $.ajax({
             data: {
                 comment: comment
             },
             type: 'POST',
             url: '/process_comments'
-        // On success of AJAX call
+        // On success of AJAX request
         }).done(function (data) {
             // Clear the textarea after new comment submitted
             $('#comment').val('');
+            // If error occurred
             if (data.error) {
                 console.log(data.error);
+            // Success
             } else {
-                // Append the message entered TODO: this will need to be the DB messages; static for now
-                console.log('SUCCESS');
+                // Append the message entered
                 $('#comment-append').append('<div class="msg_bubble"><strong>' + custName +': </strong>' + comment + '</div>');
             }
-        });
+        }); // END .done()
+        // Prevent default form submission event
         event.preventDefault();
-    });
+
+    }); // END $('form')
 }); // END $.ready()
