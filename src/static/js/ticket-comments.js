af53549434e1844b6677446ca221db54ce43a490
kaistullich <kairstullich@gmail.com>
2017-05-16 17:08:40 -0700

    removed checking if div is empty, not needed

--- src/static/js/ticket-comments.js
+++ src/static/js/ticket-comments.js
@@ -1,13 +1,7 @@
 $(document).ready(function () {
     // Get Name of the user
     var custName = $('#first-name').text();
-    // Variable for the comment=append div
-    var commentAppend = $('#comment-append');
-    // Check if the div is empty
-    if (commentAppend.html() === '') {
-        // If empty display message
-        commentAppend.append('<i><h4 id="com-placeholder">No comments so far...</h4></i>');
-    }
+
     // On click of button
     $('form').on('submit', function (event) {
         // Grab `box` ID
@@ -16,8 +10,7 @@ $(document).ready(function () {
         divBox.scrollTop = divBox.scrollHeight;
         // Grab input entered
         var comment = $('#comment').val();
-        // Remove the placeholder comment
-        $('#com-placeholder').remove();
+
         // Start AJAX
         $.ajax({
             data: {
@@ -30,10 +23,11 @@ $(document).ready(function () {
             // Clear the textarea after new comment submitted
             $('#comment').val('');
             if (data.error) {
-                console.log(data.error)
+                console.log(data.error);
             } else {
                 // Append the message entered TODO: this will need to be the DB messages; static for now
-                commentAppend.append('<div class="msg_bubble"><strong>' + custName +': </strong>' + comment + '</div>');
+                console.log('SUCCESS');
+                $('#comment-append').append('<div class="msg_bubble"><strong>' + custName +': </strong>' + comment + '</div>');
             }
         });
         event.preventDefault();
